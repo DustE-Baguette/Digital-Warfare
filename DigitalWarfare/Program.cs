6e279f0022a80379327827f8f07982a76a1ad6bd
Dominykas Jaksta <domjaksta@gmail.com>
2025-06-08 15:06:11 +0100

    Added more advanced button building and printing, with tweaks to text processing

--- DigitalWarfare/Program.cs
+++ DigitalWarfare/Program.cs
@@ -4,60 +4,37 @@
     {
         static void Main(string[] args)
         {
+            int selectedOption = 0; 
+
             ConsoleTweaks.MoveWindowToCenter();
             Console.Title = "DIGITAL WARFARE";
 
-            // Prints out text one char at a time to simulate being typed out
-            void PrintOut(string text, ConsoleColor colour, int delay)
-            {
-                Console.ForegroundColor = colour;
+            
 
-                foreach (char c in text)
-                {
-                    Console.Write(c);
-                    Thread.Sleep(delay); // Delay in ms
-                }
-
-                Console.ResetColor();
-            }
-
-            // Prints out text but centred
-            void PrintCentreText(string text, ConsoleColor colour, int delay)
-            {
-                Console.ForegroundColor = colour;
-
-                var lines = text.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
-
-                // Simple 1 line strings
-                if (lines.Length == 0)
-                {
-                    Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (text.Length / 2)) + "}", text));
-                }
-
-                // Complicated mulit-line strings
-                foreach (var line in lines)
-                {
-                    int leftPadding = Math.Max((Console.WindowWidth - line.Length) / 2, 0);
-                    Console.Write(new string(' ', leftPadding));
-
-                    foreach (char c in line)
-                    {
-                        Console.Write(c);
-                    }
-                    Console.WriteLine();
-                }
-
-                Console.ResetColor();
-            }
+            
 
             string title = " _____    __   ______   __   ______  ______   __           __     __   ______   ______   ______  ______   ______   ______    \r\n/\\  __-. /\\ \\ /\\  ___\\ /\\ \\ /\\__  _\\/\\  __ \\ /\\ \\         /\\ \\  _ \\ \\ /\\  __ \\ /\\  == \\ /\\  ___\\/\\  __ \\ /\\  == \\ /\\  ___\\   \r\n\\ \\ \\/\\ \\\\ \\ \\\\ \\ \\__ \\\\ \\ \\\\/_/\\ \\/\\ \\  __ \\\\ \\ \\____    \\ \\ \\/ \".\\ \\\\ \\  __ \\\\ \\  __< \\ \\  __\\\\ \\  __ \\\\ \\  __< \\ \\  __\\   \r\n \\ \\____- \\ \\_\\\\ \\_____\\\\ \\_\\  \\ \\_\\ \\ \\_\\ \\_\\\\ \\_____\\    \\ \\__/\".~\\_\\\\ \\_\\ \\_\\\\ \\_\\ \\_\\\\ \\_\\   \\ \\_\\ \\_\\\\ \\_\\ \\_\\\\ \\_____\\ \r\n  \\/____/  \\/_/ \\/_____/ \\/_/   \\/_/  \\/_/\\/_/ \\/_____/     \\/_/   \\/_/ \\/_/\\/_/ \\/_/ /_/ \\/_/    \\/_/\\/_/ \\/_/ /_/ \\/_____/ \r\n";
-            PrintCentreText(title, ConsoleColor.Blue, 30);
 
-            for (int i = 0; i < Console.WindowWidth; i++)
+            Console.WriteLine(new string('=', Console.WindowWidth)); 
+
+            TextTweaks.TypeOut("lognbk asdjklasd aqwieoiqwbn hjasllas", ConsoleColor.Magenta, 35);
+            Console.WriteLine();
+
+            var buttonEven = TextTweaks.BuildButton("What");
+            var buttonOdd = TextTweaks.BuildButton("whatr");
+
+            Console.WriteLine();
+
+            foreach (var line in buttonOdd)
             {
-                Console.Write("=");
+                Console.WriteLine(TextTweaks.CentreLine(line));
             }
 
+            TextTweaks.PrintButtonAt("WoahMan", 35, 35, ConsoleColor.DarkGreen);
+
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ DigitalWarfare/TextTweaks.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalWarfare
+{
+    public class TextTweaks
+    {
+        // Prints out text one char at a time to simulate being typed out
+        public static void TypeOut(string text, ConsoleColor colour, int delay)
+        {
+            Console.ForegroundColor = colour;
+
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(delay); // Delay in ms
+            }
+
+            Console.ResetColor();
+        }
+
+        // Returns single line text thats centred
+        public static string CentreLine(string text)
+        {
+            return (string.Format("{0," + ((Console.WindowWidth / 2) + (text.Length / 2)) + "}", text));
+        }
+
+        public static string[] CentreGroup(string text)
+        {
+            var lines = text.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+            // Complicated mulit-line strings
+            foreach (var line in lines)
+            {
+                int leftPadding = Math.Max((Console.WindowWidth - line.Length) / 2, 0);
+                Console.Write(new string(' ', leftPadding));
+
+                foreach (char c in line)
+                {
+                    Console.Write(c);
+                }
+                Console.WriteLine();
+            }
+
+            return lines;
+        }
+
+        public static string[] BuildButton(string text)
+        {
+            if (text.Length % 2 == 0) // Even length
+            {
+                double padding = (16 - text.Length);
+                padding = Math.Round(padding / 2);
+
+                string caps = "+----------------+";
+                string middle = '|' + new string(' ', (int)padding) + text + new string(' ', (int)padding) + '|';
+
+                return new[] { caps, middle, caps };
+            }
+            else // Odd length
+            {
+                double padding = (15 - text.Length);
+                padding = Math.Round(padding / 2);
+
+                string caps = "+---------------+";
+                string middle = '|' + new string(' ', (int)padding) + text + new string(' ', (int)padding) + '|';
+
+                return new[] {caps, middle, caps };
+            }
+        }
+
+        public static void PrintButtonAt(string text, int x, int y, ConsoleColor colour)
+        {
+            Console.ForegroundColor = colour;
+
+            var buttonLines = BuildButton(text);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.SetCursorPosition(x, y + i);
+                Console.Write(buttonLines[i]);
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
